28d3c80129a96e945cb64d05d0b8ba79e8bbac1c
Christopher Janzon <christopher.janzon@gmail.com>
2024-07-16 00:13:24 +0200

    update example flake

--- examples/raspberry-pi/flake.nix
+++ examples/raspberry-pi/flake.nix
@@ -7,7 +7,6 @@
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
-    nixos-hardware.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs@{ self, nixpkgs, nixos-tow-boot, disko, nixos-hardware, ... }: {
@@ -15,62 +14,37 @@
     nixosConfigurations = {
       example = inputs.nixpkgs.lib.nixosSystem {
         system = "aarch64-linux";
-        #system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
         modules = [
           nixos-hardware.nixosModules.raspberry-pi-4
           nixos-tow-boot.nixosModules.default
           disko.nixosModules.disko
           ({ config, pkgs, lib, ... }: {
             system.stateVersion = lib.versions.majorMinor lib.version;
-            boot.initrd.systemd.enableTpm2 = false; # https://github.com/NixOS/nixos-hardware/issues/858
-            #boot.isContainer = true;
-            environment.systemPackages =
-            let
-              pkgsFor-x86_64-linux = import nixpkgs { system = "x86_64-linux"; };
-            in [
-              pkgsFor-x86_64-linux.qemu_full
-              (pkgs.writeScriptBin "eh" ''
-                ${pkgsFor-x86_64-linux.wrapQemuBinfmtP "qemu-aarch64-linux-binfmt-P" "${pkgsFor-x86_64-linux.qemu}/bin/qemu-aarch64"}
-              '')
-              pkgs.util-linux
-            ];
-            boot.initrd.availableKernelModules = [
-              "xhci_pci"
-              "nvme"
-              "usb_storage"
-              "sd_mod"
-            ];
-            #boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
-            nixpkgs.config.allowUnsupportedSystem = true;
-            boot.firmware.tow-boot.raspberry-pi = {
-              enable = true;
-              devices = [ "4b" ];
-              #files.test = "/test";
-              installOnActivation = true;
-            };
-            boot.consoleLogLevel = 15;
-            boot.initrd.systemd = {
-              enable = true;
-              emergencyAccess = true;
-            };
-            boot.loader.generic-extlinux-compatible = {
-              enable = true;
-            };
-            boot.loader.grub = {
-              enable = false;
-              devices = [ "nodev" ];
-              efiSupport = true;
-            };
-            #boot.loader.systemd-boot = {
-            #  enable = true;
-            #};
-            virtualisation.vmVariant = {
-              virtualisation = {
-                #graphics = true;
-                diskSize = 1024 * 8;
-                memorySize = 4096; # A bit more memory is usually needed when building on target (i.e. using flake)
-                #diskImage = null; # Disable persistant storage
-                #emptyDiskImages = [ (1024 * 5) ]; # Add one 5 GiB disk to install onto
+            boot = {
+              consoleLogLevel = 15;
+              initrd = {
+                systemd = {
+                  enable = true;
+                  emergencyAccess = true;
+                  enableTpm2 = false; # https://github.com/NixOS/nixos-hardware/issues/858
+                };
+              };
+              firmware.tow-boot.raspberry-pi = {
+                enable = true;
+                devices = [ "4b" ];
+                installOnActivation = true;
+              };
+              loader = {
+                generic-extlinux-compatible = {
+                  enable = true;
+                };
+                grub = {
+                  enable = false;
+                  devices = [ "nodev" ];
+                  efiSupport = true;
+                };
+                systemd-boot.enable = false;
               };
             };
             disko.devices.disk.main ={
@@ -117,7 +91,6 @@
             };
           })
         ];
-        specialArgs = { inherit inputs; };
       };
     };
   };
